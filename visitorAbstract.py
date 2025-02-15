9753d1ed7c186364359cdd7120bf7f8fc0b07115
Ana Maria <anamariadecarvalhomendonca51@gmail.com>
2025-02-15 16:42:59 -0300

    começando a parte de expressões

--- visitorAbstract.py
+++ visitorAbstract.py
@@ -25,4 +25,58 @@ class AbstractVisitor(metaclass = ABCMeta):
     @abstractmethod
     def visitDeclaracaoGlobalComposta(self, declaracao):
         pass
-   
+
+    #Começando a parte de expressões...
+    @abstractmethod
+    def vistAnd(self, e):
+        pass
+
+    @abstractmethod
+    def visitOr(self, ou):
+        pass
+    
+    @abstractmethod
+    def visitEquals(self, igual):
+        pass
+
+    @abstractmethod
+    def visitDiferent(self, diferente):
+        pass
+
+    @abstractmethod
+    def visitMaiorQue(self, maior):
+        pass
+
+    @abstractmethod
+    def visitMenor(self,menor):
+        pass
+
+    @abstractmethod
+    def visitMenorIgual(self,menorIgual):
+        pass
+
+    @abstractmethod
+    def visitMaiorIgual(self, maiorIgual):
+        pass
+
+    @abstractmethod
+    def visitSoma(self, soma):
+        pass
+
+    @abstractmethod
+    def visitSub(self, sub):
+        pass
+
+    @abstractmethod
+    def visiMult(self, mult):
+        pass
+
+    @abstractmethod
+    def visitDiv(self, div):
+        pass
+
+    @abstractmethod
+    def visitMod(self, mod):
+        pass
+
+    #Como fazer para o unário????
