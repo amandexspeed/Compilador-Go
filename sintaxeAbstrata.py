d9c2589d97e0538e4933e2d8473a5f641a266a60
JPedro021 <howtoplayer0@gmail.com>
2025-02-27 19:09:39 -0300

    Update sintaxeAbstrata.py
    Classes abstratas e concretas do IF, FOR e ELSE introduzidas

--- sintaxeAbstrata.py
+++ sintaxeAbstrata.py
@@ -126,3 +126,112 @@ class DeclaracaoGlobalCompostaConcrete(DeclaracaoGlobal):
 
     def accept(self, visitor):
         return visitor.visitDeclaracaoGlobalComposta(self)
+
+class EstruturaIF(metaclass = ABCMeta):
+    @abstractmethod
+    def accept(self, visitor):
+        pass
+
+class EstruturaIFconcrete(EstruturaIF):
+    def __init__(self, expressao, codigo):
+        self.expressao = expressao
+        self.codigo = codigo
+
+    def accept(self, visitor):
+        return visitor.visitEstruturaIFconcrete(self)
+
+class EstruturaIF_ELSEconcrete(EstruturaIF):
+    def __init__(self, expressao, codigo, expressao_else):
+        self.expressao = expressao
+        self.codigo = codigo
+        self.expressao_else = expressao_else
+    
+    def accept(self, visitor):
+        return visitor.visitEstruturaIF_ELSEconcrete(self)
+        
+
+class EstruturaELSE(metaclass = ABCMeta):
+    @abstractmethod
+    def accept(self, visitor):
+        pass
+
+class EstruturaELSEconcrete(EstruturaELSE):
+    def __init__(self, codigo):
+        self.codigo = codigo
+
+    def accept(self, visitor):
+        return visitor.visitEstruturaELSEconcrete(self)
+
+class EstruturaELSE_IFconcrete(EstruturaELSE):
+    def __init__(self, estrutura_if):
+        self.estrutura_if = estrutura_if
+
+    def accept(self, visitor):
+        return visitor.visitEstruturaELSE_IFconcrete(self)
+        
+
+class For_CLIKE(metaclass = ABCMeta):
+    @abstractmethod
+    def accept(self, visitor):
+        pass
+
+class For_CLIKEconcrete(For_CLIKE):
+    def __init__(self, declaracao, expressao1, expressao2, codigo):
+        self.declaracao = declaracao
+        self.expressao1 = expressao1
+        self.expressao2 = expressao2
+        self.codigo = codigo
+
+    def accept(self,visitor):
+        return visitor.visitFor_CLIKEconcrete(self)
+
+class For_INFINITO(metaclass = ABCMeta):
+    @abstractmethod
+    def accept(self, visitor):
+        pass
+
+class For_INFINITOconcrete(For_INFINITO):
+    def __init__(codigo):
+        self.codigo = codigo
+
+    def accept(self,visitor):
+        return visitor.visitFor_INFINITOconcrete(self)
+
+class For_WHILE(metaclass = ABCMeta):
+    @abstractmethod
+    def accept(self, visitor):
+        pass
+
+class For_WHILEconcrete(For_WHILE):
+    def __init__(self, expressao, codigo):
+        self.expressao = codigo
+        self.codigo = codigo
+
+    def accept(self, visitor):
+        return visitor.visitFor_WHILEconcrete(self)
+
+class EstruturaFOR(metaclass = ABCMeta):
+    @abstractmethod
+    def accept(self, visitor):
+        pass
+
+class EstruturaFOR_CLIKE(EstruturaFOR):
+    def __init__(self, for_clike):
+        self.for_clike = for_clike
+
+    def accept(self, visitor):
+        return visitor.visitEstruturaFOR_CLIKE(self)
+
+class EstruturaFOR_INFINITO(EstruturaFOR):
+    def __init__(self, for_infinito):
+        self.for_infinito = for_infinito
+
+    def accept(self, visitor):
+        return visitor.visitEstruturaFOR_CLIKE(self)
+
+class EstruturaFOR_WHILE(EstruturaFOR):
+    def __init__(self, for_while):
+        self.for_while = for_while
+
+    def accept(self, visitor):
+        return visitor.visitEstruturaFOR_CLIKE(self)
