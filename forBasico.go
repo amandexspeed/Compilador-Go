0876358f641c8130316437bb55b8ca53732b3e07
Amanda de Jesus Melo <60000299+amandexspeed@users.noreply.github.com>
2025-04-13 00:52:53 -0300

    Correção for básico

--- forBasico.go
+++ forBasico.go
@@ -2,26 +2,23 @@ package main
 
 var (
     a float64 = 1.0
-    a float64 = 1.0
     b string
     c bool
 )
 
-func main() {
+func main(){
 	i := 0
 	for (i<10) {
-	for (i<10) {
 		i++
-	}	
+	}
 	funcif()	
 }
 
 func funcif(){
 
 	i,j:=0,1;
-	i,j:=0,1;
 	if (true){
-		if i==0 { 
+
 		if i==0 { 
 			i++;
 		}else{
